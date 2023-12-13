16b1f2db9a753ac427b757784cdbfba21ee3f0c3
PabloAndroidStudio <palcgut2303@g.educaand.es>
2023-12-13 17:01:47 +0100

    Commit 17:01

--- app/src/main/java/com/example/tarea3_4_a/ui/listacompra/ListaCompraViewModel.java
+++ app/src/main/java/com/example/tarea3_4_a/ui/listacompra/ListaCompraViewModel.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.AndroidViewModel;
 
 import com.example.tarea3_4_a.basedatos.BaseDatosApp;
@@ -13,11 +12,11 @@ import com.example.tarea3_4_a.entidades.Producto;
 import java.io.Closeable;
 import java.util.List;
 
-public class ListaCompraViewModel extends ViewModel {
+public class ListaCompraViewModel extends AndroidViewModel  {
 
     private final LiveData<List<Producto>> productos;
     public ListaCompraViewModel(@NonNull Application application) {
-        super((Closeable) application);
+        super(application);
         //Inicializamos el contenido de la lista, al de la tabla de la base de datos
         productos = BaseDatosApp
                 .getInstance(application)
